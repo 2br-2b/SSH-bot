eb63a6c8b02896dd4b2846e4a31f396080439e4d
CLASSIFIED <847785bd-d466-47cd-a536-eae4096d241d@anonaddy.me>
2020-05-15 13:45:37 -0500

    Added documentation

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import dmlistener
 from discord.ext import commands
 import discord
 
+#create the bot
 bot = commands.Bot(">>>")
 
+#add the cog to listen for commands
 dml = dmlistener.dmlistener(bot)
 bot.add_cog(dml)
 
 @bot.event
 async def on_ready():
     print("Started!")
+    #set the bot's status
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=" for `>>>`"))
 
+#run the bot
 bot.run(token_manager.TOKEN)
